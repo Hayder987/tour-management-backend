bdb4a74a2c08b21ff51a60139b3afbeaf22a1ee3
hayder <hayderbd4290@gmail.com>
2025-07-08 21:30:56 +0600

    create user schema and routes

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,14 @@
 import express from "express";
 import { Request, Response } from 'express';
+import { UserRoutes } from "./app/modules/user/user.route";
+import cors from "cors"
 
 const app = express();
+
+app.use(express.json());
+app.use(cors())
+
+app.use("/api/v1", UserRoutes)
 
 app.get('/', (req:Request, res:Response)=>{
   res.json({
