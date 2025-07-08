fc258393a429f7dd1cab7132d692220bb335c899
hayder <hayderbd4290@gmail.com>
2025-07-08 21:57:02 +0600

    create clean route

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,14 @@
 import express from "express";
 import { Request, Response } from 'express';
-import { UserRoutes } from "./app/modules/user/user.route";
 import cors from "cors"
+import { router } from "./app/routes";
 
 const app = express();
 
 app.use(express.json());
 app.use(cors())
 
-app.use("/api/v1", UserRoutes)
+app.use("/api/v1", router)
 
 app.get('/', (req:Request, res:Response)=>{
   res.json({
--- src/app/modules/user/user.controller.ts
+++ src/app/modules/user/user.controller.ts
@@ -8,8 +8,11 @@ const createUser = async (req: Request, res: Response) => {
   try {
     const { name, email } = req.body;
     const user = await User.create({ name, email });
-    res.status(httpStatus.CREATED);
-    
+    res.status(httpStatus.CREATED).json({
+        message: "user create successfully",
+        user
+    });
+
   } catch (error: any) {
     console.log(error);
     res.status(httpStatus.BAD_REQUEST).json({
